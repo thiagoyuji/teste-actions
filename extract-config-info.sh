19553c839e3454b84e47af8dc18fadd06dfedfe3
Thiago Yuji <tyhayaci@gmail.com>
2025-03-13 20:46:25 -0300

    Update extract-config-info.sh

--- extract-config-info.sh
+++ extract-config-info.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+AREA=$1
+
+FILE_PATH="$AREA.json"
+
+if [ ! -f "$FILE_PATH"] then
+  echo "File not found!"
+  exit 1
+fi
+
+AREA=$(jq -r '.area' "$FILE_PATH")
+echo "area=$AREA" >> $GITHUB_OUTPUT
