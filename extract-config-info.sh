dbe3e6027e0cbdde5203d3886baf680f14350fa3
Thiago Yuji <tyhayaci@gmail.com>
2025-03-14 00:16:17 -0300

    Update extract-config-info.sh

--- extract-config-info.sh
+++ extract-config-info.sh
@@ -3,7 +3,13 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+if [ -z "$2" ]; then
+  echo "Error: Trabalho argument is missing."
+  exit 1
+fi
+
 AREA=$1
+TRABALHO=$2
 
 FILE_PATH="$AREA.json"
 
@@ -23,3 +29,18 @@ echo "zonas_batch=$DEV_BATCH_ZONES" >> "$GITHUB_OUTPUT"
 
 FUNCIONALIDADES=$(jq -c '.funcionalidades' "$FILE_PATH")
 echo "funcionalidades=$FUNCIONALIDADES" >> "$GITHUB_OUTPUT"
+
+EXCEPTION_EXISTS=$(jq --arg job "$TRABALHO" '.["process-exceptions"][] | select(.trabalho == $job)' "$JSON_FILE")
+
+if [ -z "$EXCEPTION_EXISTS" ]; then
+  echo "Job $JOB_TO_CHECK is not in exceptions. Skipping."
+  echo "skip_job=true" >> $GITHUB_ENV
+else
+  echo "Job $JOB_TO_CHECK is in exceptions. Proceeding."
+  # Extrai as zonas online ou batch
+  ONLINE_ZONES=$(echo "$EXCEPTION_EXISTS" | jq -r '.zonas.dev.online | @csv')
+  BATCH_ZONES=$(echo "$EXCEPTION_EXISTS" | jq -r '.zonas.dev.batch | @csv')
+  echo "online_zones=$ONLINE_ZONES" >> $GITHUB_ENV
+  echo "batch_zones=$BATCH_ZONES" >> $GITHUB_ENV
+  echo "skip_job=false" >> $GITHUB_ENV
+fi
