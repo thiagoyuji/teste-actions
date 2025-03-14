6655b3556b31404ff95a090f143b0740a2f2637e
Thiago Yuji <tyhayaci@gmail.com>
2025-03-14 00:26:52 -0300

    Update extract-config-info.sh

--- extract-config-info.sh
+++ extract-config-info.sh
@@ -1,11 +1,15 @@
 if [ -z "$1" ]; then
+
   echo "Error: Area argument is missing."
   exit 1
+  
 fi
 
 if [ -z "$2" ]; then
+
   echo "Error: Trabalho argument is missing."
   exit 1
+  
 fi
 
 AREA=$1
@@ -14,8 +18,10 @@ TRABALHO=$2
 FILE_PATH="$AREA.json"
 
 if [ ! -f "$FILE_PATH" ]; then
+
   echo "Error: File '$FILE_PATH' not found."
   exit 1
+  
 fi
 
 AREA_FROM_FILE=$(jq -r '.area' "$FILE_PATH")
@@ -30,17 +36,20 @@ echo "zonas_batch=$DEV_BATCH_ZONES" >> "$GITHUB_OUTPUT"
 FUNCIONALIDADES=$(jq -c '.funcionalidades' "$FILE_PATH")
 echo "funcionalidades=$FUNCIONALIDADES" >> "$GITHUB_OUTPUT"
 
-EXCEPTION_EXISTS=$(jq --arg job "$TRABALHO" '.["process-exceptions"][] | select(.trabalho == $job)' "$JSON_FILE")
+EXCEPTION_EXISTS=$(jq --arg job "$TRABALHO" '.["processar_excecoes"][] | select(.trabalho == $job)' "$FILE_PATH")
 
 if [ -z "$EXCEPTION_EXISTS" ]; then
-  echo "Job $JOB_TO_CHECK is not in exceptions. Skipping."
-  echo "skip_job=true" >> $GITHUB_ENV
+
+  echo "e_excecao=false" >> "$GITHUB_OUTPUT"
+  
 else
-  echo "Job $JOB_TO_CHECK is in exceptions. Proceeding."
-  # Extrai as zonas online ou batch
-  ONLINE_ZONES=$(echo "$EXCEPTION_EXISTS" | jq -r '.zonas.dev.online | @csv')
-  BATCH_ZONES=$(echo "$EXCEPTION_EXISTS" | jq -r '.zonas.dev.batch | @csv')
-  echo "online_zones=$ONLINE_ZONES" >> $GITHUB_ENV
-  echo "batch_zones=$BATCH_ZONES" >> $GITHUB_ENV
-  echo "skip_job=false" >> $GITHUB_ENV
+
+  echo "e_excecao=true" >> "$GITHUB_OUTPUT"
+
+  EXCEPTION_DEV_ONLINE_ZONES=$(echo "$EXCEPTION_EXISTS" | jq -c '.zonas.dev.online')
+  echo "zonas_online_excecao=$DEV_ONLINE_ZONES" >> "$GITHUB_OUTPUT"
+  
+  EXCEPTION_DEV_BATCH_ZONES=$(echo "$EXCEPTION_EXISTS" | jq -c '.zonas.dev.batch')
+  echo "zonas_batch_excecao=$DEV_BATCH_ZONES" >> "$GITHUB_OUTPUT"
+
 fi
